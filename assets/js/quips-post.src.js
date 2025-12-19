26dc99d6a7105b3ebb636d448fba3fb5df3ab2a4
Russell Heimlich <kingkool68@gmail.com>
2025-12-18 20:31:42 -0500

    Add textarea character counter

--- assets/js/quips-post.src.js
+++ assets/js/quips-post.src.js
@@ -1,5 +1,15 @@
 import domReady from '@wordpress/dom-ready';
 
 domReady(function () {
-	console.log( 'hello quips' );
+
+	// Character counter
+	var textarea = document.getElementById('the-quip');
+	var charCount = document.getElementById('the-character-count');
+	var segmenter = new Intl.Segmenter('en-US', {
+		granularity: 'grapheme',
+	});
+
+	textarea.addEventListener('input', function () {
+		charCount.textContent = [...segmenter.segment(this.value)].length;
+	});
 });
